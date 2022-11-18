d94bef6db5bda1a1492b1efcdc2de186dc8988fe
Bubaflip <m.minvielle1@gmail.com>
2022-11-18 12:29:56 +0000

    final fix

--- components/Transaction.js
+++ components/Transaction.js
@@ -10,87 +10,94 @@ let switchingChain = false;
 
 const Transaction = ({ children }) => {
 
-    const { address, isConnected } = useAccount();
-    const provider = useProvider();
-    const [signer, setSigner] = useState(null);
-    const { connect, connectors } = useConnect();
-    const { chain } = useNetwork();
-    const [whitelisted, setWhitelisted] = useState(0);
-    const { switchNetwork, status } = useSwitchNetwork({
-        onError(error) {
-            console.log('Error Switch Network', error)
-        },
-        chainId: 1
-    });
-    const { disconnect } = useDisconnect();
+  const { address, isConnected } = useAccount();
+  const provider = useProvider();
+  const [signer, setSigner] = useState(null);
+  const { connect, connectors } = useConnect();
+  const { chain } = useNetwork();
+  const [whitelisted, setWhitelisted] = useState(0);
+  const { switchNetwork, status } = useSwitchNetwork({
+    onError(error) {
+      console.log('Error Switch Network', error)
+    },
+    chainId: 1
+  });
+  const { disconnect } = useDisconnect();
 
-    // fetch signature
-    const fetcher = (url, triedKey) => fetch(url,
-        triedKey
-    ).then((res) => res.json());
+  // fetch signature
+  const fetcher = (url, triedKey) => fetch(url,
+    triedKey
+  ).then((res) => res.json());
 
-    const { data, error } = useSWR('/api/staticdata?input=' + address, fetcher);
+  const { data, error } = useSWR('/api/staticdata?input=' + address, fetcher);
 
-    const checkAddress = () => {
-      if (list[address.toLowerCase()] == null || list[address.toLowerCase()] == undefined) {
-        setWhitelisted(0)
-      } else {
-        setWhitelisted(1);
-      }
+  const checkAddress = () => {
+    console.log(address.toLowerCase());
+    // const reducedList = Object.keys(list).reduce((total, key) => {
+    //   return {
+    //     ...total,
+    //     [key.toLowerCase()]: list[key],
+    //   }
+    // }, {});
+    if (list[address.toLowerCase()] === null || list[address.toLowerCase()] === undefined) {
+      setWhitelisted(0)
+    } else {
+      setWhitelisted(1);
     }
+  }
 
-    // set up signer 
-    const setUpSigner = () => {
-        const { ethereum } = window;
-        if (ethereum) {
-            const provider = new ethers.providers.Web3Provider(ethereum);
-            const providerSigner = provider.getSigner();
-            setSigner(providerSigner);
-        }
+  // set up signer 
+  const setUpSigner = () => {
+    const { ethereum } = window;
+    if (ethereum) {
+      const provider = new ethers.providers.Web3Provider(ethereum);
+      const providerSigner = provider.getSigner();
+      setSigner(providerSigner);
     }
+  }
 
-    // ask to switch chain if wrong chain and status changes
-    useEffect(() => {
-        if (chain && chain.id !== 1 && switchingChain === false && switchNetwork) {
-            switchingChain = true;
-            switchNetwork(1);
-            checkAddress();
-        }
-    }, [switchNetwork, status]);
+  // ask to switch chain if wrong chain and status changes
+  useEffect(() => {
+    if (chain && chain.id !== 1 && switchingChain === false && switchNetwork) {
+      switchingChain = true;
+      switchNetwork(1);
+      checkAddress();
+    }
+  }, [switchNetwork, status]);
 
 
-    // return the total supply and user owned token when chain switches and completed
-    useEffect(() => {
-        if (chain && chain.id === 1 && signer) {
-            switchingChain = false;
-        } else if (chain && !signer && chain.id === 1) {
-            setUpSigner();
-            checkAddress();
-        }
-    }, [chain, signer]);
+  // return the total supply and user owned token when chain switches and completed
+  useEffect(() => {
+    if (chain && chain.id === 1 && signer) {
+      switchingChain = false;
+    } else if (chain && !signer && chain.id === 1) {
+      setUpSigner();
+      checkAddress();
+    }
+  }, [chain, signer]);
 
-    return (
-        <>
-            <TransactionProvider.Provider
-                value={{
-                    wallet: {
-                        address: address,
-                        isConnected: isConnected,
-                        provider: provider,
-                        connect: connect,
-                        connectors: connectors,
-                        switchNetwork: switchNetwork,
-                        chain: chain,
-                        sigData: data,
-                        disconnect: disconnect,
-                        whitelisted: whitelisted,
-                    },
-                }}
-            >
-                {children}
-            </TransactionProvider.Provider>
-        </>
-    );
+  return (
+    <>
+      <TransactionProvider.Provider
+        value={{
+          wallet: {
+            address: address,
+            isConnected: isConnected,
+            provider: provider,
+            connect: connect,
+            connectors: connectors,
+            switchNetwork: switchNetwork,
+            chain: chain,
+            sigData: data,
+            disconnect: disconnect,
+            whitelisted: whitelisted,
+          },
+        }}
+      >
+        {children}
+      </TransactionProvider.Provider>
+    </>
+  );
 }
 
 export default Transaction;
